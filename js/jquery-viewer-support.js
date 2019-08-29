9cc51adcc4a290ca47697fc008eebf7e9b253e40
Michal P <43756311+michpcx@users.noreply.github.com>
2019-08-29 21:20:28 +0100

    Starting to add individual pages for countries

---
+++ js/jquery-viewer-support.js
@@ -0,0 +1,92 @@
+$(function () {
+  'use strict';
+
+  var console = window.console || { log: function () {} };
+  var $images = $('.docs-pictures');
+  var $toggles = $('.docs-toggles');
+  var $buttons = $('.docs-buttons');
+  var options = {
+    // inline: true,
+    url: 'data-original',
+    ready: function (e) {
+      console.log(e.type);
+    },
+    show: function (e) {
+      console.log(e.type);
+    },
+    shown: function (e) {
+      console.log(e.type);
+    },
+    hide: function (e) {
+      console.log(e.type);
+    },
+    hidden: function (e) {
+      console.log(e.type);
+    },
+    view: function (e) {
+      console.log(e.type);
+    },
+    viewed: function (e) {
+      console.log(e.type);
+    }
+  };
+
+
+
+  $images.on({
+    ready:  function (e) {
+      console.log(e.type);
+    },
+    show:  function (e) {
+      console.log(e.type);
+    },
+    shown:  function (e) {
+      console.log(e.type);
+    },
+    hide:  function (e) {
+      console.log(e.type);
+    },
+    hidden: function (e) {
+      console.log(e.type);
+    },
+    view:  function (e) {
+      console.log(e.type);
+    },
+    viewed: function (e) {
+      console.log(e.type);
+    }
+  }).viewer(options);
+
+
+
+  $toggles.on('change', 'input', function () {
+    var $input = $(this);
+    var name = $input.attr('name');
+
+    options[name] = name === 'inline' ? $input.data('value') : $input.prop('checked');
+    $images.viewer('destroy').viewer(options);
+  });
+
+  $buttons.on('click', 'button', function () {
+    var data = $(this).data();
+    var args = data.arguments || [];
+
+    if (data.method) {
+      if (data.target) {
+        $images.viewer(data.method, $(data.target).val());
+      } else {
+        $images.viewer(data.method, args[0], args[1]);
+      }
+
+      switch (data.method) {
+        case 'scaleX':
+        case 'scaleY':
+          args[0] = -args[0];
+          break;
+      }
+    }
+  });
+
+
+  $('[data-toggle="tooltip"]').tooltip();
+});
